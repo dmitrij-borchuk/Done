32587d84f6e435a456b92aa8d76ec4f90d1a7ab6
Dmytro Borchuk <dborchuk@remit.se>
2018-10-03 00:03:49 +0300

    Creat task

--- src/app/new-task-form/new-task-form.component.ts
+++ src/app/new-task-form/new-task-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import TasksService from '../tasks.service';
+import Task from '../task';
 
 @Component({
   selector: 'app-new-task-form',
   templateUrl: './new-task-form.component.html',
   styleUrls: ['./new-task-form.component.css']
 })
-export class NewTaskFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class NewTaskFormComponent {
+  constructor(private tasksService: TasksService) {
   }
 
+  onEnter(title) {
+    this.tasksService.add(new Task(title));
+  }
 }
